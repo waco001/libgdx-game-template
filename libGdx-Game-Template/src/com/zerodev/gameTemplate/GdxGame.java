91bef39bd0ecc5f8202e0f7ff70da750ef7f209f
Zerod159@gmail.com <Zerod159@gmail.com@ce20b4a0-bea8-ba58-89de-139ef4aaf531>
2012-08-01 21:26:01 +0000

    Some Changes

--- libGdx-Game-Template/src/com/zerodev/gameTemplate/GdxGame.java
+++ libGdx-Game-Template/src/com/zerodev/gameTemplate/GdxGame.java
@@ -11,80 +11,51 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.zerodev.gameTemplate.screens.*;
 
 public class GdxGame implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private TextureRegion region;
-	//private Sprite sprite;
-	Button myButton;
+
+	private Screen currentScreen;
+	
 	
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera();
-		camera.setToOrtho(true);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		region = new TextureRegion(texture, 0, 0, 512, 275);
-		region.flip(false, true);
-		//sprite = new Sprite(region);
-		//sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		//sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		//sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-		Texture buttonTexture = new Texture(Gdx.files.internal("img/button/buttonupdown.png"));
-		
-		
-		
-		myButton = new Button(new TextureRegion(buttonTexture,0,0,100,20),new TextureRegion(buttonTexture,0,20,100,20));
-		
+		currentScreen = new ScreenMenu(this);
 		
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+		currentScreen.dispose();
 	}
 
-	float x = 0.45f;
 	
 	@Override
 	public void render() {	
-		ButtonStyle style = new ButtonStyle();
-		
-		if(myButton.isPressed)
-			myButton.setStyle(style);
-
-		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		//sprite.draw(batch);
-		batch.draw(region,0,0);
-		
-		
-		myButton.draw(batch, 1);
-		batch.end();
+		currentScreen.update();
+		currentScreen.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
+		currentScreen.resize(width, height);
 	}
 
 	@Override
 	public void pause() {
+		currentScreen.pause();
 	}
 
 	@Override
 	public void resume() {
+		currentScreen.resume();
+	}
+	
+	
+	public void setScreen(Screen screen)
+	{
+		currentScreen.dispose();
+		screen.load();
+		currentScreen = screen;
 	}
 }
---
+++ libGdx-Game-Template/src/com/zerodev/gameTemplate/screens/ScreenGame.java
@@ -0,0 +1,27 @@
+package com.zerodev.gameTemplate.screens;
+
+import com.zerodev.gameTemplate.GdxGame;
+
+public class ScreenGame extends Screen{
+	public ScreenGame(GdxGame game)
+	{
+		super(game);
+		
+	}
+	
+	
+	public void load() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+}
